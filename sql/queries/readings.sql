58565eb43dab7b88ed55d50467ae706a217aae4b
Keith <4restrei@gmail.com>
2026-07-09 15:03:35 +0300

    files...

---
+++ sql/queries/readings.sql
@@ -0,0 +1,9 @@
+-- name: AddReading :one
+INSERT INTO readings(id, patient_id, time_of_reading, glucose, basal_rate, bolus, carbs, exercise_duration, exercise_intensity)
+VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
+RETURNING *;
+
+-- name: SendReadingToModel :exec
+UPDATE readings
+SET in_the_model=true
+WHERE id=$1;
