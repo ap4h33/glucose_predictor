51f2b61de29d7476d110271c46561c09f9274a25
Keith <4restrei@gmail.com>
2026-07-19 14:43:01 +0300

    create array of all predictions

--- main/handler_recommendation.go
+++ main/handler_recommendation.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"time"
 
+	"github.com/ap4h33/glucose_predictor/internal/database"
 	"github.com/go-chi/chi/v5"
 )
 
@@ -28,6 +30,67 @@ func (apiCfg apiConfig) handlerGetRecommendations(w http.ResponseWriter, r *http
 	respondWithJSON(w, 200, recommendation)
 }
 
-func (apiCfg apiConfig) handlerCreateRecommendations(w http.ResponseWriter, r *http.Request) {
+func (apiCfg apiConfig) handlerSendInforForRecommendations(w http.ResponseWriter, r *http.Request, patientID int32) {
+	type response struct {
+		Readings         []database.Reading                `json:"readings"`
+		ModelPredictions []database.GetModelPredictionsRow `json:"model_predictions"`
+		OduPredictions   []database.GetModelPredictionsRow `json:"odu_predictions"`
+	}
+
+	type PredictionResponse struct {
+		ModelPrediction database.GetModelPredictionsRow
+		ModelName       string `json:"model_name"`
+	}
+
+	now := time.Now()
+
+	// AIModel :=
+	// oduModel :=
+
+	aiPredictions, err := apiCfg.DB.GetModelPredictions(
+		r.Context(),
+		database.GetModelPredictionsParams{
+			// ModelID: AIModel,
+			PatientID:     patientID,
+			TimePredicted: now,
+		},
+	)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Could not get model predictions: %s", err))
+		return
+	}
+
+	oduPredictions, err := apiCfg.DB.GetModelPredictions(
+		r.Context(),
+		database.GetModelPredictionsParams{
+			// ModelID: oduModel,
+			PatientID:     patientID,
+			TimePredicted: now,
+		},
+	)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Could not get model predictions: %s", err))
+		return
+	}
+
+	var predictions []PredictionResponse
+	// This creates an array of all of the predictions, and labals them with according names.
+	for _, p := range aiPredictions {
+		predictions = append(predictions, PredictionResponse{
+			ModelPrediction: p,
+			ModelName:       "AI",
+		})
+	}
+
+	for _, p := range oduPredictions {
+		predictions = append(predictions, PredictionResponse{
+			ModelPrediction: p,
+			ModelName:       "ODU",
+		})
+	}
 
 }
+
+// func (apiCfg apiConfig) handlerCreateRecommendations(w http.ResponseWriter, r *http.Request) {
+
+// }
